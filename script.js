f86bf1dabeae0f989efa6a0a2f197de2fcede6d2
DimaOstanin <xnadab@gmail.com>
2024-01-26 03:40:27 +0200

    adding first snake

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("WOrk)");
+
+const board = document.getElementById("game-board")
+
+let snake = [{x:10,y:10}]
+
+function draw(){
+    board.innerHTML = '';
+    drawSnake();
+
+}
+
+function drawSnake() {
+    snake.forEach((segment) => {
+      const snakeElement = createGameElement('div', 'snake');
+      setPosition(snakeElement, segment);
+      board.appendChild(snakeElement);
+    });
+  }
+  
+
+function createGameElement(tag,className){
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+function setPosition(element ,position){
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+    console.log(element.style.gridColumn,element.style.gridRow)
+}
+
+draw();
